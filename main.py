02b986ebdc1a1b0bc27865b619fca8e1320faa22
kaielibro <kaielibro@gmail.com>
2024-06-02 09:47:31 -0400

    Added report function and a bunch of other adjustments

--- main.py
+++ main.py
@@ -10,15 +10,33 @@ def get_char_count(text):
     chars = {}
     lc_text = text.lower()
     for i in lc_text:
-        chars[i] = chars.get(i, 0) + 1
+        if i.isalpha():
+            chars[i] = chars.get(i, 0) + 1
     return chars
 
-def main():
-    book_path = "books/frankenstein.txt"
+def sort_on(dict):
+    return dict["num"]
+
+def dict_to_list_dict(dict):
+    list_dict = []
+    for key, value in dict.items():
+        list_dict.append({"char": key, "num": value})
+    return list_dict
+
+def print_report(book_path):
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     char_count = get_char_count(text)
-    print(f"The word count is: {word_count}")
-    print(f"The character counts are: {char_count}")
+    char_list = dict_to_list_dict(char_count)
+    char_list.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    for char in char_list:
+        print(f"The '{char}' character was found ")
+    print("--- End report ---")
+
+def main():
+    book_path = "books/frankenstein.txt"
+    print_report(book_path)
 
 main()
